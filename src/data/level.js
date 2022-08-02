c7489175ef8b34fb57258c1798c29db62be34b27
Levine Hua <951995489@qq.com>
2022-08-02 09:58:16 +0800

    [feat]: 调整段位等级，降低难度，发现以前难度太高了

--- src/data/level.js
+++ src/data/level.js
@@ -3,48 +3,48 @@
  * @Author: Levine
  * @Date: 2022-07-28 14:46:35
  * @LastEditors: Levine
- * @LastEditTime: 2022-07-28 14:55:53
+ * @LastEditTime: 2022-08-02 09:48:59
  * @FilePath: /papapa/src/data/level.js
  */
 // 所有等级
 const LEVELS = [
   {
     min: 0,
-    max: 499999,
+    max: 99999,
     name: '青铜'
   },
   {
-    min: 500000,
-    max: 999999,
+    min: 100000,
+    max: 199999,
     name: '白银'
   },
   {
-    min: 1000000,
-    max: 1499999,
+    min: 200000,
+    max: 299999,
     name: '黄金'
   },
   {
-    min: 1500000,
-    max: 1999999,
+    min: 300000,
+    max: 399999,
     name: '铂金'
   },
   {
-    min: 2000000,
-    max: 2499999,
+    min: 400000,
+    max: 499999,
     name: '钻石'
   },
   {
-    min: 2500000,
-    max: 2999999,
+    min: 500000,
+    max: 599999,
     name: '大神'
   },
   {
-    min: 3000000,
-    max: 3499999,
+    min: 600000,
+    max: 699999,
     name: '无敌'
   },
   {
-    min: 3500000,
+    min: 700000,
     max: 0,
     name: '传说'
   }
